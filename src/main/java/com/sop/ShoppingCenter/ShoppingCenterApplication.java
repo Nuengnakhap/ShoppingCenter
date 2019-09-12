e94a8bc65d88d2af5a9ed40e5cfe80bc3e277e55
nuengnakhap <nueng1569@hotmail.com>
2019-09-12 10:10:26 +0700

    create project

---
+++ src/main/java/com/sop/ShoppingCenter/ShoppingCenterApplication.java
@@ -0,0 +1,13 @@
+package com.sop.ShoppingCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCenterApplication.class, args);
+	}
+
+}
