3e0f3140f41460d3fd9b08997e22293e6814b02b
nuengnakhap <nueng1569@hotmail.com>
2019-10-17 09:37:11 +0700

    update sql account

--- src/main/java/com/sop/ShoppingCenter/ShoppingCenterApplication.java
+++ src/main/java/com/sop/ShoppingCenter/ShoppingCenterApplication.java
@@ -3,9 +3,11 @@ package com.sop.ShoppingCenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableResourceServer
 public class ShoppingCenterApplication {
 
 	public static void main(String[] args) {
