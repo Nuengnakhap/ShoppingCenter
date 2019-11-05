230e7fa19ecd8649028338424c0dda2b1e1315be
nuengnakhap <nueng1569@hotmail.com>
2019-11-05 14:22:48 +0700

    update product screen

--- src/components/product/ProductBox.js
+++ src/components/product/ProductBox.js
@@ -3,11 +3,29 @@ import PropTypes from "prop-types";
 import "../../assets/css/global.css";
 import "../../assets/css/store.css";
 import ProductCart from "./ProductCart";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fas, fab, far);
 
 function ProductBox(props) {
   return (
-    <div className="content-inner">
-      <div className="title">{props.title}</div>
+    <div className="content-inner" style={{ marginBottom: "-25px" }}>
+      {!!props.title && <div className="title">{props.title}</div>}
+      <div className="content-search">
+        <form action="">
+          <input type="search" />
+          <FontAwesomeIcon
+            className="search"
+            icon={["fas", "search"]}
+            color="black"
+            style={{ margin: "0 2px", fontSize: "2vmin" }}
+          />
+        </form>
+      </div>
       <div className="content-product">
         {!!props.products &&
           props.products.map((item, index) => (
@@ -16,7 +34,7 @@ function ProductBox(props) {
               title="My Product"
               stars={4.5}
               onPress={() => props.updateCart(1)}
-              style={{ width: "25vmin" }}
+              // style={{ width: "33%" }}
             />
           ))}
       </div>
@@ -26,6 +44,7 @@ function ProductBox(props) {
 
 ProductBox.propTypes = {
   title: PropTypes.string,
+  searchBox: PropTypes.bool,
   products: PropTypes.array
 };
 
