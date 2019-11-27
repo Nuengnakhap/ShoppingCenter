e3696fb8d83c3d6e02089790a1cc61577db53c4c
Onez <nueng1569@hotmail.com>
2019-11-28 03:10:28 +0700

    update production

--- src/main/java/com/sop/ShoppingCenter/ShoppingCenterApplication.java
+++ src/main/java/com/sop/ShoppingCenter/ShoppingCenterApplication.java
@@ -3,11 +3,12 @@ package com.sop.ShoppingCenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 //@EnableResourceServer
-public class ShoppingCenterApplication {
+public class ShoppingCenterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCenterApplication.class, args);
